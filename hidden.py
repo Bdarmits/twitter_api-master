f834d789624a955a98741491c9afe397653e2282
Bohdan <darmits@ucu.edu.ua>
2018-02-18 20:57:14 +0200

    Add files via upload

---
+++ hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "i0JOQVctCEOJhAsINXSffcvku",
+            "consumer_secret": "0JMD9SOKiHJDuf7xtYnH8s5aeJ2dttT2VmVVzMoy3mtGPfw8In",
+            "token_key": "839189378018729986-yXEBuCXVkJUGtXK8CW8PEngr36ltOkY",
+            "token_secret": "wWrnFb3kg4jeoqplLPQf073TQzh2PBwMG3VicUDjJQffp"}
